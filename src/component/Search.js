2712867bfbb940e5b2f64b8fc9df5534849c03ec
nohgijin <ynda11111@naver.com>
2020-11-11 22:53:00 +0900

    [#4] feat: redirect search url

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,10 +1,32 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import { Redirect } from "react-router";
 import {} from "../styles/search.scss";
 
 const Search = () => {
+  const [searchState, setSearchState] = useState({ search: false });
+  const [termState, setTermState] = useState({ term: "" });
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      setSearchState({ search: true });
+      setTermState({ term: event.target.value });
+    }
+  }
+
   return (
     <Fragment>
-      <input type="text" className="search" placeholder="🔍 Search Crawler" />
+      <input
+        type="text"
+        className="search"
+        placeholder="🔍 Search Crawler"
+        onKeyPress={handleKeyPress}
+      />
+      {searchState.search ? (
+        <Redirect
+          to={{ pathname: "/result", state: { results: termState.term } }}
+        />
+      ) : (
+        ""
+      )}
     </Fragment>
   );
 };
---
+++ src/pages/result.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Result = () => {
+  return (
+    <div className="result">
+      <div>hihihi</div>
+    </div>
+  );
+};
+
+export default Result;
